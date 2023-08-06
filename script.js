16df3f945266d1e55cf70fa0ca9fc78ba658226c
mohit <interactivecodemohit@gmail.com>
2023-08-06 13:28:14 +0530

    first commit upload all file

---
+++ script.js
@@ -0,0 +1,180 @@
+$(document).ready(function() {
+
+    $("#clk").click(function(){
+        $("nav").toggle("slow");
+        $("nav").css("position: 'absolute'");
+        // $( ".banner" ).css('margin-top','200px');
+        
+    });
+
+
+    $("#HAIRCUT").click(function() {
+      
+        $(".blk").text("Clipper Cut");
+        $(".price-1").text("$30");
+        $(".clps").text("A haircut using clippers to achieve an ultra-short design. (30 min)");
+
+        $(".hair").text("Haircut");
+        $(".price-2").text("$39");
+        $(".hairct").text("A haircut, trim or shape on anyone over the age of 10. (60 min)");
+        
+        $(".sign").text("Signature Haircut");
+        $(".price-3").text("$49");
+        $(".signct").text("A haircut, trim, shape on anyone over the age of 10 with a customized conditioning treatment. (1 hr 15 min)");
+
+
+        $(".kid").text("Kids Haircut");
+        $(".price-4").text("$20");
+        $(".kidhct").text("A haircut on a child age 10 & under.(30 min)");
+
+        $(".bng").text("Bang Trim");
+        $(".price-5").text("$19");
+        $(".bngt").text("A trim on the bang area. Shampoo, conditioner and scalp massage not included. (15 min)");
+
+        $(".bng").text("Bang Trim");
+        $(".price-6").text("$19");
+        $(".bngt").text("A trim on the bang area. Shampoo, conditioner and scalp massage not included. (15 min)");
+
+        });
+    $("#STYLING").click(function() {
+      
+        $(".blk").text("Brush");
+        $(".price-1").text("$25");
+        $(".clps").text("Blow dry using brush, strait styling. (30 min)");
+
+        $(".hair").text("Curly Brush");
+        $(".price-2").text("$28");
+        $(".hairct").text("Blow dry using brush, strait styling. (30 min)");
+        
+        $(".sign").text("Light Hair Styling");
+        $(".price-3").text("$44");
+        $(".signct").text("A haircut, trim, shape on anyone over the age of 10 with a customized conditioning treatment. (1 hr 15 min)");
+
+
+        $(".kid").text("Curly Hair");
+        $(".price-4").text("$38");
+        $(".kidhct").text("A haircut on a child age 10 & under.(30 min)");
+
+        // $(".bng").text("Bang Trim");
+        // $(".price-5").text("$19");
+        // $(".bngt").text("A trim on the bang area. Shampoo, conditioner and scalp massage not included. (15 min)");
+
+        // $(".bng").text("Bang Trim");
+        // $(".price-6").text("$19");
+        // $(".bngt").text("A trim on the bang area. Shampoo, conditioner and scalp massage not included. (15 min)");
+
+        });
+    $("#COLOUR").click(function() {
+      
+        $(".blk").text("Balayage");
+        $(".price-1").text("$150");
+        $(".clps").text("A haircut using clippers to achieve an ultra-short design. (30 min)");
+
+        $(".hair").text("Bleach & Tone");
+        $(".price-2").text("$90");
+        $(".hairct").text("A haircut, trim or shape on anyone over the age of 10. (60 min)");
+        
+        $(".sign").text("One Color");
+        $(".price-3").text("$80");
+        $(".signct").text("A haircut, trim, shape on anyone over the age of 10 with a customized conditioning treatment. (1 hr 15 min)");
+
+
+        $(".kid").text("Foliage");
+        $(".price-4").text("$110");
+        $(".kidhct").text("A haircut on a child age 10 & under.(30 min)");
+
+        // $(".bng").text("Bang Trim");
+        // $(".price-5").text("$19");
+        // $(".bngt").text("A trim on the bang area. Shampoo, conditioner and scalp massage not included. (15 min)");
+
+        // $(".bng").text("Bang Trim");
+        // $(".price-6").text("$19");
+        // $(".bngt").text("A trim on the bang area. Shampoo, conditioner and scalp massage not included. (15 min)");
+
+        });
+    $("#TREATMENT").click(function() {
+      
+        $(".blk").text("Olaplex");
+        $(".price-1").text("$25");
+        $(".clps").text("Olaplex treatment, number 2 and number 3. (20 min)");
+
+        $(".hair").text("Orising Treatment");
+        $(".price-2").text("$28");
+        $(".hairct").text("Hair treatment using Orising hair care products. (60 min)");
+        
+        $(".sign").text("Label Treatment");
+        $(".price-3").text("$80");
+        $(".signct").text("Hair treatment using Label cosmetics, consists of 12 steps. (1 hr 15 min)");
+
+
+        $(".kid").text("Foliage");
+        $(".price-4").text("$110");
+        $(".kidhct").text("A haircut on a child age 10 & under.(30 min)");
+
+        // $(".bng").text("Bang Trim");
+        // $(".price-5").text("$19");
+        // $(".bngt").text("A trim on the bang area. Shampoo, conditioner and scalp massage not included. (15 min)");
+
+        // $(".bng").text("Bang Trim");
+        // $(".price-6").text("$19");
+        // $(".bngt").text("A trim on the bang area. Shampoo, conditioner and scalp massage not included. (15 min)");
+
+        });
+
+
+
+});
+
+
+$(document).ready(function() {
+
+$("#my_img1").click(function() {
+    $("#display_img").attr({"src": "images/galimage1.jpg"});
+
+   });
+$("#my_img2").click(function() {
+    $("#display_img").attr({"src": "images/galimage3.jpg"});
+
+   });
+$("#my_img3").click(function() {
+    $("#display_img").attr({"src": "images/image1.jpg"});
+
+   });
+$("#my_img4").click(function() {
+    $("#display_img").attr({"src": "images/image2.jpg"});
+
+   });
+$("#my_img5").click(function() {
+    $("#display_img").attr({"src": "images/image3.jpg"});
+
+   });
+$("#my_img6").click(function() {
+    $("#display_img").attr({"src": "images/image4.jpg"});
+
+   });
+$("#my_img7").click(function() {
+    $("#display_img").attr({"src": "images/image5.jpg"});
+
+   });
+$("#my_img8").click(function() {
+    $("#display_img").attr({"src": "images/image6.jpg"});
+
+   });
+$("#my_img9").click(function() {
+    $("#display_img").attr({"src": "images/image7.jpg"});
+
+   });
+$("#my_img10").click(function() {
+    $("#display_img").attr({"src": "images/image8.jpg"});
+
+   });
+$("#my_img11").click(function() {
+    $("#display_img").attr({"src": "images/image9.jpg"});
+
+   });
+
+  
+
+});
+
+
